f27baa5893c22336a791527680b65dc414f3dae2
Khan-Slava <vaceslavhan22@gmail.com>
2023-10-23 23:55:18 +0600

    module07

---
+++ Module07.Homework/ClassLibrary1/Tank.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyClassLib
+{
+    
+    public class Tank
+    {
+        
+        string name { get ; set; }
+        private int Maneuverability { get; set; }
+        private int ammunition { get; set; }
+        private int armor { get; set; }
+
+        public Tank(string name,Random rnd)
+        {
+            
+            this.name = name;
+            this.Maneuverability =GetRand(rnd);
+            this.armor = GetRand(rnd);
+            this.ammunition = GetRand(rnd);
+
+        }
+        public int GetRand(Random rnd)
+        {
+            
+            int num = rnd.Next(0, 100);
+            return num;
+        }
+        public void GetSpecifications() {
+
+            Console.WriteLine( "Имя: {0} -> Маневренность: {1} -> Уровени брони: {2} -> Урон: {3}",name,Maneuverability,ammunition,armor);
+        }
+        public string GetName()
+        {
+
+            return name;
+                }
+        public static Tank operator * (Tank tank1,Tank tank2) 
+        {
+            try {//если танк 1 превосходит по какой - то величине, то его счетчик увеличивается и уменьшается в противном случае
+                //если счетчик в минусе, то танк проиграл
+                int count = 0;
+                if (tank1.Maneuverability > tank2.Maneuverability)
+                {
+                    count++;
+                }
+                else count--;
+                
+                if (tank1.armor > tank2.armor)
+                {
+                    count++;
+                }
+                else count--;
+                
+                if (tank1.ammunition > tank2.ammunition)
+                {
+                    count++;
+                }
+                else count--;
+
+                if (count > 0) {
+                    return tank1;
+                }
+                else return tank2;
+            }
+            catch {
+                return null;
+            }
+            
+        }
+    }
+}
---
+++ Module07.Homework/ConsoleApp1/Program.cs
@@ -0,0 +1,53 @@
+﻿using MyClassLib;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Module7_Homework
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Random rand = new Random();
+            List<Tank> T34 = new List<Tank>();
+            Console.WriteLine("T-34:");
+            for (int i = 0; i < 5; i++)
+            {
+                string name = "T34 - "+i.ToString();
+
+                T34.Add ( new Tank(name,rand));
+                T34[i].GetSpecifications();
+            }
+            string separator = new string('-', 15);
+            Console.WriteLine(separator);
+
+            Console.WriteLine("Pantera:");
+            List<Tank> Pantera = new List<Tank>();
+
+            for (int i = 0; i < 5; i++)
+            {
+                string name = "Pantera - " + i.ToString();
+                Pantera.Add( new Tank(name, rand));
+                Pantera[i].GetSpecifications();
+            }
+
+            
+            List<Tank> WonTanks = new List<Tank>();
+            for (int i = 0; i < 5; i++)
+            {
+                Console.WriteLine(separator);
+
+                Console.WriteLine("{0} VS {1}", T34[i].GetName(), Pantera[i].GetName());
+                WonTanks.Add ( T34[i]*Pantera[i]);
+
+                Console.WriteLine("Победитель: {0}",WonTanks[i].GetName());
+            }
+
+
+
+        }
+    }
+}
