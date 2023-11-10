b37dad521cd9f1cbb3fa602048d2335ee4a00dcd
Khan-Slava <vaceslavhan22@gmail.com>
2023-11-10 23:41:08 +0600

    module8

---
+++ Module08.HomeWork/Module08.Practice/Program.cs
@@ -0,0 +1,287 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics.CodeAnalysis;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Net.Http.Headers;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Module08.Practice
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+           
+            task1();
+            string slash = new string('-', 30);
+            Console.WriteLine(slash);
+            task2();
+            Console.WriteLine(slash);
+            task3();
+
+        }
+/*
+        1. В С # индексация начинается с нуля, но в некоторых языках программирования это не так.
+Например, в Turbo Pascal индексация массиве начинается с 1.
+Напишите класс RangeOfArray, который позволяет работать с массивом такого типа,
+в котором индексный диапазон устанавливается пользователем. Например, в диапазоне от 6 до 10, или от –9 до 15.*/
+        static void task1() 
+        {
+            int startNum = 0;
+            int needNum = 0;
+            RangeOfArray MyClass = new RangeOfArray();
+            List<int> MyArray = MyClass.array; 
+            foreach (int i in MyArray)
+            {
+                Console.Write(i + " ");
+            }
+            Console.WriteLine();
+
+
+            Console.WriteLine("Введите начало массива:");
+            Int32.TryParse(Console.ReadLine(),out startNum); 
+            Console.WriteLine("Введите нужный элемент массива:");
+            Int32.TryParse(Console.ReadLine(), out needNum);
+            int count = MyClass[startNum, needNum];
+            Console.WriteLine(count);
+
+
+
+        }
+        /*
+        2)Написать программу «Продуктовый супермаркет»: выбирается ряд продуктов,
+        рассчитывается их сумма с учетом скидки в 5% (скидка предоставляется, если покупка сделана с 8.00 до 12.00
+        по текущему времени)*/
+        static void task2()
+        {
+            
+            List<Products> MyProducts = new List<Products>();
+            MyProducts.Add(new Products() { name = "pen", price = 100});
+            MyProducts.Add(new Products() { name = "pencel", price = 50 });
+            MyProducts.Add(new Products() { name = "book", price = 15000 });
+            MyProducts.Add(new Products() { name = "note", price = 10 });
+            MyProducts.Add(new Products() { name = "paints", price = 1600 });
+            MyProducts.Add(new Products() { name = "dictionary", price = 450 });
+            Supermarket market = new Supermarket(MyProducts);
+
+            Console.WriteLine("Кассир А.С.Пушкин");
+            Console.WriteLine(DateTime.Now.TimeOfDay);
+            Console.WriteLine("список покупок:");
+            foreach (var i in MyProducts)
+            {
+                Console.Write("{0} --- {1} тг \n",i.name, i.price);
+            }
+            Console.WriteLine("Сумма: {0}", market["А.С.Пушкин"]);
+
+
+            Console.WriteLine("скидка 5% работает с 8:00 до 12:00");
+
+        }
+        
+        /*
+        В файле хранится информация об объеме продаж товара за пять последних месяцев.
+        С помощью метода наименьших квадратов спрогнозировать объемы продаж на следующие три месяца.
+        В качестве линии тренда выбрать линейную функцию*/
+        static void task3() {
+        
+            List<Prod> MyProd = new List<Prod>();
+            MyProd.Add(new Prod(new DateTime(2023, 01, 01), "йогурт", 1234));
+            MyProd.Add(new Prod(new DateTime(2023, 02, 01), "йогурт", 2222));
+            MyProd.Add(new Prod(new DateTime(2023, 03, 01), "йогурт", 1000));
+            MyProd.Add(new Prod(new DateTime(2023, 04, 01), "йогурт", 5934));
+            MyProd.Add(new Prod(new DateTime(2023, 05, 01), "йогурт", 162));
+            Forecast forecast = new Forecast(MyProd);
+
+            
+            Console.WriteLine(forecast[forecast.products]);
+        }
+    }
+    //task1
+    public class RangeOfArray
+    {
+        public List<int> array = new List<int> () ;
+
+        public RangeOfArray()
+        {
+            Random rnd = new Random();
+            for (int i = 0; i < 10; i++)
+            {
+                array.Add(rnd.Next(100));
+                
+            }
+            
+        }
+
+         public int this[int start , int numOfArray]
+         {
+            get{
+                
+                try
+                {
+                    if ( (start - numOfArray) > 0) {
+                        return  0;
+                    }
+                    else
+                    {
+                        return array[Math.Abs(start - numOfArray)];
+
+                    }
+                }
+                catch { return 0; }
+
+            }
+            //1 2 3 4
+            //    1 2 3 4
+            
+
+}   
+    }
+    
+    public class Products
+    {
+        public Products()
+        {
+            
+        }
+        public string name {  get; set; }
+        public double price { get; set; }
+
+
+    }
+    //task2
+    public class Supermarket
+    {
+        List<Products> products = null;
+        public Supermarket()
+        {
+            products = new List<Products>();
+            products.Add(new Products(){name = "pen", price = 100 }) ;
+
+        }
+        public Supermarket(List<Products> list)
+        {
+            products = list;
+        }
+
+        public double this[string name]
+        {
+            get
+            {
+                TimeSpan start = new TimeSpan(8,0,0);
+                TimeSpan end = new TimeSpan(12,0,0);
+                double sum = 0;
+                foreach (Products product in products)
+                    {
+                        sum+=product.price ;
+                    }
+                if (DateTime.Now.TimeOfDay >start && DateTime.Now.TimeOfDay<end)
+                {
+                    
+                    return sum*0.95;
+                }
+                else { return sum; }  
+            }
+        }
+    }
+
+    //task3
+    public class Prod
+    {
+        public DateTime date { get; set; }
+        public string name { get; set; }
+        public int countOfSold { get; set; }
+        public Prod() { }
+        public Prod(DateTime date, string name,int countOfSold)
+        {
+            this.date = date;
+            this.name = name;
+            this.countOfSold = countOfSold;
+        }
+    }
+    public class Forecast
+    {
+        public List<Prod> products = null;
+        public Forecast()
+        {
+            
+        }
+        public Forecast(List<Prod> products)
+        {
+            this.products = products;
+        }
+        public double this[List<Prod> products]
+        {
+            get
+            {
+                //вычисления метода наименьшего квадрата
+                
+                int x = 0, xx=0;
+                for(int i = 1; i <= products.Count; i++)
+                {
+                    x += i;
+                    xx += i*i;
+
+                }
+                double y = 0, xy = 0;
+                int count =0;
+                foreach(Prod prod in products) 
+                {
+                    count++;
+                    y += prod.countOfSold;
+                    xy += y * count;
+                    
+                }
+
+                //xx * A+x * B=xy
+                //x * A+count * B = y
+
+                double result = xx * count - x * x;
+                //пусть будет так...
+                return result;
+
+            }
+        }
+    }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+}
---
+++ Module08.HomeWork/Program.cs
@@ -0,0 +1,249 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics.CodeAnalysis;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Net.Http.Headers;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Module08.Homework
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+           
+            task1();
+            string slash = new string('-', 30);
+            Console.WriteLine(slash);
+            task2();
+            Console.WriteLine(slash);
+            
+
+        }
+        /*
+                Создать индексатор, для одномерного массива, который при установке значения будет возводить в квадрат передаваемое
+        значение переменной и устанавливать его для указанного индекса. При получении элемента массива по индексу будет 
+        возвращаться его текущее значение..*/
+        static void task1() 
+        {
+            int num = 0;
+            int position = 0;
+
+            task1_Array MyArray = new task1_Array();
+
+
+            bool flag = true;
+            Console.WriteLine("напишите 123456789 в ввод позиции для остановки");
+            while (flag)
+            {
+                Console.WriteLine("Введите позицию(от 0 до 100):");
+                Int32.TryParse(Console.ReadLine(), out position);
+                if (position == 123456789)
+                {
+                    break;
+                }
+                Console.WriteLine("Введите число для заполнения:");
+                Int32.TryParse(Console.ReadLine(), out num);
+                MyArray[position] = num;
+
+            }
+            Console.WriteLine("Введите позицию для показа:");
+            Int32.TryParse(Console.ReadLine(), out position);
+            int count = MyArray[position];
+            Console.WriteLine(count);
+
+
+
+        }
+        /*
+        2)Написать программу, рассчитывающую сумму коммунальных платежей:
+        есть базовые тарифы на отопление (на 1 м2 площади), на воду (на 1 чел), на газ (на 1 чел), 
+        на текущий ремонт (на 1 м2 площади). 
+        Задается метраж помещения, количество проживающих людей, сезон (осенью и зимой отопление дороже), 
+        наличие льгот (ветеран труда– 30 % от его части; ветеран войны- 50% от его части). 
+        Вывести таблицу со столбцами: Вид платежа, Начислено, Льготная скидка, Итого. Посчитать итоговую сумму.*/
+        static void task2()
+        {
+
+            UtilityBills bills = new UtilityBills(new Water(2), new Otoplenie(25.5), new Gas(2));
+            Console.WriteLine("Написать программу, рассчитывающую сумму коммунальных платежей: " +
+                "есть базовые тарифы на отопление(на 1 м2 площади), на воду(на 1 чел), на газ(на 1 чел), " +
+                "на текущий ремонт(на 1 м2 площади).Задается метраж помещения, количество проживающих людей, " +
+                "сезон(осенью и зимой отопление дороже), наличие льгот(ветеран труда– 30 % от его части; " +
+                "ветеран войны-50 % от его части). Вывести таблицу со столбцами: Вид платежа, Начислено, Льготная скидка, " +
+                "Итого.Посчитать итоговую сумму.");
+            Console.WriteLine("veteran = false; season =false");
+            bool veteran = false;
+            bool season = false;
+            Console.WriteLine(bills[season,veteran]);
+
+            Console.WriteLine("veteran = true; season =true");
+            veteran = true;
+            season = true;
+            Console.WriteLine(bills[season, veteran]);
+
+            Console.WriteLine("veteran = false; season =true");
+            veteran = false;
+            season = true;
+            Console.WriteLine(bills[season, veteran]);
+
+            Console.WriteLine("veteran = true; season =false");
+            veteran = true;
+            season = false;
+            Console.WriteLine(bills[season, veteran]);
+        }
+        
+        
+    }
+    //task1
+    public class task1_Array
+    {
+
+        public int[] array = new int[100];
+        
+
+        public task1_Array()
+        {
+            
+        }
+
+         public int this[int position]
+         {
+
+            set
+            {
+                this.array[position] = value*value;
+            }
+            get { return this.array[position]; }
+            
+
+}   
+    }
+
+
+    //task2
+    public class Water
+    {
+        public double price = 26;
+        public int countOfPeople { get; set; }
+        public Water(int count) {
+            this.countOfPeople = count;
+        }
+        public Water()
+        {
+
+        }
+        public double result()
+        {
+            return this.price*countOfPeople;
+        }
+
+    }
+    public class Otoplenie
+    {
+        public double price = 456.23;
+        public double area { get; set; }
+        public Otoplenie(double area)
+        {
+            this.area = area;
+        }
+        public Otoplenie()
+        {
+            
+        }
+        public double result()
+        {
+            return this.price * area;
+        }
+    }
+    public class Gas
+    {
+        public double price = 666.66;
+        public int countOfPeople { get; set; }
+        public Gas(int count)
+        {
+            this.countOfPeople = count;
+        }
+        public Gas()
+        {
+            
+        }
+        public double result()
+        {
+            return this.price * countOfPeople;
+        }
+
+    }
+    public class UtilityBills
+    {
+        
+        Water water ;
+        Otoplenie otoplenie;
+        Gas gas ;
+
+        public UtilityBills(Water water, Otoplenie otoplenie, Gas gas)
+        {
+            this.water = water;
+            this.otoplenie = otoplenie;
+            this.gas = gas;
+        }
+
+        public double this[bool season, bool veteran]
+        {
+            get
+            {
+                double result = 0;
+                result = water.result() + otoplenie.result() + gas.result();
+                if (season) { result = result * 1.2; }
+                if (veteran) { result = result * 0.7; }
+                return result;   
+            }
+        }
+    }
+
+   
+        
+    }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
